82cd326c86031b9f02d28af3092f983c25bdca7f
Oli Evans <oli@tableflip.io>
2019-02-08 14:25:01 +0000

    fix: make it  work
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './filecoin-logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="">
+        <header className="">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.

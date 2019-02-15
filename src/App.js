6ffce7bb2a2f59f3c09aeca91de6c5b0bbc2d58a
Alan Shaw <alan.shaw@protocol.ai>
2019-02-15 14:57:29 +0000

    feat: fetch leaderboard from URL
    Specify `REACT_APP_API_URL=<url>` when running `npm start` or `npm build` to customise the API URL that the leaderboard will be fetched from.
    
    By default it'll attempt to get it from the static `leaderboard.json` file in the `public` dir.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import numeral from 'numeral'
 import logo from './filecoin-logo.svg'
-import LeaderboardData from './leaderboard.json'
 import gold from './gold.png'
 import silver from './silver.png'
 import bronze from './bronze.png'
+
+const API_URL = process.env.REACT_APP_API_URL || '/leaderboard.json'
+const REFRESH_INTERVAL = process.env.REACT_APP_REFRESH_INTERVAL || 10 * 1000
+
+const Header = () => (
+  <header className="mw7 center pl5">
+    <h1 className='ma0 pv4 fw2 f1 montserrat'>
+      <img src={logo} alt='fil' width='80' className='db dib-l mb3 mb0-l mr4 v-mid'/>
+      <span className='v-mid'>Replication challenge</span>
+    </h1>
+  </header>
+)
 
 const Avatar = ({name, className = 'ml2 mr4 v-mid', size = 60}) => {
   return (
@@ -36,8 +47,38 @@ const Medal = ({ type }) => {
 }
 
 class App extends Component {
+  state = {}
+
+  componentDidMount () {
+    const refresh = async () => {
+      try {
+        const res = await fetch(API_URL)
+        if (!res.ok) throw new Error(`unexpected status ${res.status}`)
+        this.setState({ leaderboardData: await res.json() })
+      } catch (err) {
+        console.error('failed to fetch leaderboard data', err)
+      } finally {
+        setTimeout(refresh, REFRESH_INTERVAL)
+      }
+    }
+    refresh()
+  }
+
   render() {
-    const data = LeaderboardData
+    const { leaderboardData } = this.state
+
+    if (!leaderboardData) {
+      return (
+        <div className="sans-serif white">
+          <Header />
+          <main className='tc'>
+            <p>Loading...</p>
+          </main>
+        </div>
+      )
+    }
+
+    const data = leaderboardData
       .map(d => ({ ...d, perByteTime: (d.repl_time * 1000) / d.params.size }))
       .sort((a, b) => a.perByteTime - b.perByteTime)
       .reduce((deduped, d) => {
@@ -51,37 +92,32 @@ class App extends Component {
 
     return (
       <div className="sans-serif white">
-        <header className="mw7 center pl5">
-          <h1 className='ma0 pv4 fw2 f1 montserrat'>
-            <img src={logo} alt='fil' width='80' className='db dib-l mb3 mb0-l mr4 v-mid'/>
-            <span className='v-mid'>Replication challenge</span>
-          </h1>
-        </header>
+        <Header />
         <main>
           <div className='mw7 center tr pb2'>
             <span className='f5 pr2 mr2'>Time / byte (ms)</span>
           </div>
           <ol className='ma0 lh-copy mw7 mb5 center db gray' style={{listStyleType: 'decimal'}}>
-          {data.slice(0,1).map(({ prover, perByteTime }) => (
-            <li className='tl f4 pa3 b--gold b--solid bw1 br3 relative shadow-1' style={{ backgroundColor: 'rgba(255, 183, 0, 0.75)' }}>
+          {data.slice(0,1).map(({ id, prover, perByteTime }) => (
+            <li key={id} className='tl f4 pa3 b--gold b--solid bw1 br3 relative shadow-1' style={{ backgroundColor: 'rgba(255, 183, 0, 0.75)' }}>
               <Medal type='gold' />
               <Entry name={prover} time={perByteTime} />
             </li>
           ))}
-          {data.slice(1,2).map(({ prover, perByteTime }) => (
-            <li className='tl f4 mt4 pa3 b--silver b--solid bw1 br3 relative shadow-1' style={{ backgroundColor: 'rgba(153, 153, 153, 0.75)' }}>
+          {data.slice(1,2).map(({ id, prover, perByteTime }) => (
+            <li key={id} className='tl f4 mt4 pa3 b--silver b--solid bw1 br3 relative shadow-1' style={{ backgroundColor: 'rgba(153, 153, 153, 0.75)' }}>
               <Medal type='silver' />
               <Entry name={prover} time={perByteTime} />
             </li>
           ))}
-          {data.slice(2,3).map(({ prover, perByteTime }) => (
-            <li className='tl f4 mt4 mb4 pa3 b--solid bw1 br3 relative shadow-1' style={{ borderColor: '#cd7f32', backgroundColor: 'rgba(205, 127, 50, 0.75)' }}>
+          {data.slice(2,3).map(({ id, prover, perByteTime }) => (
+            <li key={id} className='tl f4 mt4 mb4 pa3 b--solid bw1 br3 relative shadow-1' style={{ borderColor: '#cd7f32', backgroundColor: 'rgba(205, 127, 50, 0.75)' }}>
               <Medal type='bronze' />
               <Entry name={prover} time={perByteTime} />
             </li>
           ))}
-          {data.slice(3).map(({ prover, perByteTime }) => (
-            <li className='tl mt3 ph3 f4'>
+          {data.slice(3).map(({ id, prover, perByteTime }) => (
+            <li key={id} className='tl mt3 ph3 f4'>
               <Entry name={prover} time={perByteTime} />
             </li>
           ))}

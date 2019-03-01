2b9bc56d7a53502c0ce730a84f9ba395455b5258
Alan Shaw <alan.shaw@protocol.ai>
2019-03-01 16:06:45 +0000

    feat: leaderboards grouped by params (#15)
    * feat: leaderboards grouped by params
    
    Also adds standard for linting.
    
    * fix: border radius on bottom item when only 2 entries
    
    * feat: fix up time
    
    This is all the work of @olizilla I stole it from his PR because it would be too hard to merge.
    
    refs https://github.com/filecoin-project/replication-game-leaderboard/pull/13

--- src/App.js
+++ src/App.js
@@ -1,66 +1,35 @@
+/* eslint-env browser */
+
 import React, { Component } from 'react'
-import numeral from 'numeral'
-import md5 from 'md5'
 import logo from './filecoin-logo.svg'
-import gold from './gold.png'
-import silver from './silver.png'
-import bronze from './bronze.png'
+import Leaderboard from './Leaderboard'
 
 const API_URL = process.env.REACT_APP_API_URL || 'leaderboard.json'
 const REFRESH_INTERVAL = process.env.REACT_APP_REFRESH_INTERVAL || 10 * 1000
 
 const Header = () => (
-  <header className="mw7 center">
+  <header className='mw7 center'>
     <h1 className='ma0 pv4 fw2 f1 montserrat tc'>
-      <img src={logo} alt='fil' width='80' className='db dib-l center mb3 mb0-l ml0-l mr4-l v-mid'/>
+      <img src={logo} alt='fil' width='80' className='db dib-l center mb3 mb0-l ml0-l mr4-l v-mid' />
       <span className='v-mid'>Replication Game</span>
     </h1>
   </header>
 )
 
-const Avatar = ({name, className = 'mr3 v-mid', size = 60}) => {
-  return (
-    <img
-      src={`https://github.com/${name}.png?size=${size}`}
-      className={className}
-      style={{width: size/2, height: size/2}}
-      alt={`${name} avatar`}
-      onError={e => { e.target.src = `https://www.gravatar.com/avatar/${md5(name)}?d=retro&s=${size}` }} />
-  )
-}
+function processLeaderboardData (data) {
+  data = data.map(d => ({ ...d, secondsPerMBTime: d.repl_time / (d.params.size / (1024 * 1024)) }))
 
-const ReplTime = ({time}) => {
-  return (
-    <span className='dib f5 fr bg-snow black pa2 br3 lh-solid tr courier ml4'>{time}</span>
-  )
-}
+  // Group leaderboard by params ID - { id: [entries] }
+  const groups = data.reduce((groups, d) => {
+    groups[d.params.id] = (groups[d.params.id] || []).concat(d)
+    return groups
+  }, {})
 
-const Entry = ({name, time}) => {
-  return (
-    <div className='flex'>
-      <Avatar name={name} />
-      <span className='fw5 montserrat white truncate flex-auto' title={name}>{name}</span>
-      <ReplTime time={numeral(time).format('0.000e+0')} />
-    </div>
-  )
-}
-
-const Medal = ({ type }) => {
-  const srcs = { gold, silver, bronze }
-  return (
-    <img src={srcs[type]} alt={type} className='absolute dn db-m db-l' style={{ width: 45, top: -3, right: '8rem' }} />
-  )
-}
-
-function formatParams ({ typ, challenge_count, vde, degree, expansion_degree, layers }) {
-  return [
-    `Type: ${typ}`,
-    `Challenges: ${challenge_count}`,
-    `VDE: ${vde}`,
-    `Degree: ${degree}`,
-    `Expansion degree: ${expansion_degree}`,
-    `Layers: ${layers}`
-  ].join(', ')
+  // [[leaderboard], [leaderboard]]
+  return Object.values(groups)
+    .filter(l => l.length > 1) // Not really a leaderboard if only 1 person...
+    .sort((a, b) => b.length - a.length)
+    .map(board => board.sort((a, b) => a.secondsPerMBTime - b.secondsPerMBTime))
 }
 
 class App extends Component {
@@ -81,12 +50,12 @@ class App extends Component {
     refresh()
   }
 
-  render() {
+  render () {
     const { leaderboardData } = this.state
 
     if (!leaderboardData) {
       return (
-        <div className="sans-serif white">
+        <div className='sans-serif white'>
           <Header />
           <main className='tc'>
             <p>Loading...</p>
@@ -95,72 +64,22 @@ class App extends Component {
       )
     }
 
-    const data = leaderboardData
-      .map(d => ({ ...d, perByteTime: (d.repl_time * 1000) / d.params.size }))
-      .sort((a, b) => a.perByteTime - b.perByteTime)
-      .reduce((deduped, d) => {
-        const exists = deduped.some(dd => {
-          return dd.prover === d.prover &&
-            dd.repl_time === d.repl_time &&
-            dd.params.size === d.params.size
-        })
-        return exists ? deduped : deduped.concat(d)
-      }, [])
+    const leaderboards = processLeaderboardData(leaderboardData)
 
     return (
-      <div className="sans-serif white">
+      <div className='sans-serif white'>
         <Header />
         <main>
           <div className='mw7 pl3 center pb2 cf'>
-            <h2 className='f4 f3-m f3-l mv3 pl4-m pl4-l tc tl-m tl-l montserrat fw2 ttu fl-m fl-l'>Leaderboard</h2>
-            <div className='f4 f3-m f3-l mv3 pr4 fr'>
-              <div className='f6 f5-m f5-l mt1 montserrat fw2'>Time / byte (ms)</div>
-            </div>
+            <h2 className='f4 f3-m f3-l mv3 pl4-m pl4-l tc tl-m tl-l montserrat fw2 ttu fl-m fl-l'>Leaderboards</h2>
           </div>
-          <ol className='ma0 lh-copy mw7 mb5 pl3 center db gray' style={{listStyleType: 'decimal'}}>
-          {data.slice(0,1).map(({ id, prover, perByteTime, params }) => (
-            <li
-              key={id}
-              className='tl f4 mh3 pa3 b--gold b--solid bw1 br3 relative shadow-1'
-              style={{ backgroundColor: 'rgba(255, 183, 0, 0.75)' }}
-              title={formatParams(params)}>
-              <Medal type='gold' />
-              <Entry name={prover} time={perByteTime} />
-            </li>
-          ))}
-          {data.slice(1,2).map(({ id, prover, perByteTime, params }) => (
-            <li
-              key={id}
-              className='tl f4 mt4 mh3 pa3 b--silver b--solid bw1 br3 relative shadow-1'
-              style={{ backgroundColor: 'rgba(153, 153, 153, 0.75)' }}
-              title={formatParams(params)}>
-              <Medal type='silver' />
-              <Entry name={prover} time={perByteTime} />
-            </li>
-          ))}
-          {data.slice(2,3).map(({ id, prover, perByteTime, params }) => (
-            <li
-              key={id}
-              className='tl f4 mt4 mh3 mb4 pa3 b--solid bw1 br3 relative shadow-1'
-              style={{ borderColor: '#cd7f32', backgroundColor: 'rgba(205, 127, 50, 0.75)' }}
-              title={formatParams(params)}>
-              <Medal type='bronze' />
-              <Entry name={prover} time={perByteTime} />
-            </li>
-          ))}
-          {data.slice(3).map(({ id, prover, perByteTime, params }) => (
-            <li
-              key={id}
-              className='tl mt3 mh3 ph3 f4'
-              title={formatParams(params)}>
-              <Entry name={prover} time={perByteTime} />
-            </li>
-          ))}
-          </ol>
+          <div className='mw7 pl3 center pb2 cf'>
+            {leaderboards.map(l => <Leaderboard entries={l} />)}
+          </div>
         </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Leaderboard.js
@@ -0,0 +1,178 @@
+import React, { useState } from 'react'
+import numeral from 'numeral'
+import md5 from 'md5'
+import gold from './gold.png'
+import silver from './silver.png'
+import bronze from './bronze.png'
+import goldRibbon from './gold-ribbon.png'
+import silverRibbon from './silver-ribbon.png'
+import bronzeRibbon from './bronze-ribbon.png'
+import proofDrgPoRep from './proof-DrgPoRep.svg'
+import proofZigzag from './proof-Zigzag.svg'
+import proofUnknown from './proof-Unknown.svg'
+
+const Avatar = ({ name, className = 'mr3 v-mid', size = 60 }) => (
+  <img
+    src={`https://github.com/${name}.png?size=${size}`}
+    className={className}
+    style={{ width: size / 2, height: size / 2 }}
+    alt={`${name} avatar`}
+    onError={e => { e.target.src = `https://www.gravatar.com/avatar/${md5(name)}?d=retro&s=${size}` }} />
+)
+
+const ReplTime = ({ time }) => (
+  <span className='dib f5 fr bg-snow black pa2 br3 lh-solid tr courier ml3'>
+    {time.toFixed(3)}
+  </span>
+)
+
+const Entry = ({ name, time, medal }) => (
+  <div className='flex' title={`${name} took ${time.toFixed(3)}s to replicate 1MiB of data`}>
+    <Avatar name={name} />
+    <span className='fw5 montserrat white truncate flex-auto'>{name}</span>
+    {medal ? <Medal type={medal} /> : null}
+    <ReplTime time={time} />
+  </div>
+)
+
+const Medal = ({ type }) => {
+  const srcs = { gold, goldRibbon, silver, silverRibbon, bronze, bronzeRibbon }
+  const style = ['gold', 'silver', 'bronze'].includes(type)
+    ? { width: 45, height: 45, top: -6, right: 0 }
+    : { width: 45, height: 84, top: -19, right: 0 }
+  return (
+    <div className='relative w3-ns'>
+      <img src={srcs[type]} alt={type} className='absolute dn db-m db-l' style={style} />
+    </div>
+  )
+}
+
+const ProofIcon = ({ typ, className }) => {
+  let src = proofUnknown
+  if (typ === 'DrgPoRep') {
+    src = proofDrgPoRep
+  } else if (typ === 'Zigzag') {
+    src = proofZigzag
+  }
+  return <img src={src} alt='icon by Eliricon from the Noun Project' width='50' height='50' className={className} />
+}
+
+const ProofDesc = {
+  DrgPoRep: 'Depth Robust Graph Proof-of-Replication',
+  Zigzag: 'Specialized Layered Depth Robust Graph Proof-of-Replication'
+}
+
+const Header = ({ params }) => (
+  <div className='pl4'>
+    <h2 className='montserrat fw2 mv3' title={ProofDesc[params.typ] || ''}>
+      <span className='dib w5'>
+        <ProofIcon className='mr2 v-mid' typ={params.typ} />
+        <span className='v-mid mr5'>{params.typ}</span>
+      </span>
+      <span className='dib v-mid' style={{ lineHeight: '50px' }}>{numeral(params.size).format('0,0')} <small className='gray'>bytes</small></span>
+    </h2>
+    <ul className='montserrat f6 fw2 list mv3 pl0'>
+      <li className='dib-ns mr3'><span className='gray mr2'>Challenges</span> {params.challenge_count}</li>
+      <li className='dib-ns mr3'><span className='gray mr2'>VDE</span> {params.vde}</li>
+      <li className='dib-ns mr3'><span className='gray mr2'>Degree</span> {params.degree}</li>
+      <li className='dib-ns mr3'><span className='gray mr2'>Expansion degree</span> {params.expansion_degree}</li>
+      <li className='dib-ns mr3'><span className='gray mr2'>Layers</span> {params.layers}</li>
+    </ul>
+  </div>
+)
+
+const CollapsedLeaderboard = ({ entries, onExpand }) => {
+  const top3 = entries.slice(0, 3)
+  const rest = entries.slice(3)
+  return (
+    <div className='mb4 pv1 ph1 br3 shadow-3' style={{ backgroundColor: '#1E2135' }}>
+      <Header params={top3[0].params} />
+      <div className='pr4 fr' style={{ marginTop: '-2.25rem' }}>
+        <div className='f7 f6-m f6-l mt1 montserrat fw2 gray'>Repl time (s/MiB)</div>
+      </div>
+      <ol className='ma0 lh-copy mw7 mb3 pl3 center db gray' style={{ listStyleType: 'decimal' }}>
+        {top3.slice(0, 1).map(({ id, prover, secondsPerMBTime, params }) => (
+          <li
+            key={id}
+            className='tl f4 mh3 pa3 b--gold b--solid bw1 br3 br--top shadow-1'
+            style={{ backgroundColor: 'rgba(255, 183, 0, 0.75)' }}>
+            <Entry name={prover} time={secondsPerMBTime} medal='gold' />
+          </li>
+        ))}
+        {top3.slice(1, 2).map(({ id, prover, secondsPerMBTime, params }) => (
+          <li
+            key={id}
+            className={`tl f4 mh3 pa3 b--silver b--solid bw1 relative shadow-1 ${top3.length === 2 ? 'br3 br--bottom' : ''}`}
+            style={{ backgroundColor: 'rgba(153, 153, 153, 0.75)' }}>
+            <Entry name={prover} time={secondsPerMBTime} medal='silver' />
+          </li>
+        ))}
+        {top3.slice(2, 3).map(({ id, prover, secondsPerMBTime, params }) => (
+          <li
+            key={id}
+            className='tl f4 mh3 pa3 b--solid bw1 br3 br--bottom shadow-1'
+            style={{ borderColor: '#cd7f32', backgroundColor: 'rgba(205, 127, 50, 0.75)' }}>
+            <Entry name={prover} time={secondsPerMBTime} medal='bronze' />
+          </li>
+        ))}
+      </ol>
+      {rest.length ? (
+        <div className='tc mb3'>
+          <button type='button' className='montserrat f6 fw2 gray bw0 br3 ph3 pv2 bg-black-10 hover-bg-black-30 pointer' onClick={onExpand}>Show {rest.length} more</button>
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+const ExpandedLeaderboard = ({ entries, onCollapse }) => (
+  <div className='mb4 pv1 ph1 br3' style={{ backgroundColor: '#1E2135' }}>
+    <Header params={entries[0].params} />
+    <div className='pr4 fr' style={{ marginTop: '-2.25rem' }}>
+      <div className='f7 f6-m f6-l mt1 montserrat fw2 gray'>Repl time (s/MiB)</div>
+    </div>
+    <ol className='ma0 lh-copy mw7 mb3 pl3 center db gray' style={{ listStyleType: 'decimal' }}>
+      {entries.slice(0, 1).map(({ id, prover, secondsPerMBTime, params }) => (
+        <li
+          key={id}
+          className='tl f4 mh3 pa3 b--gold b--solid bw1 br3 shadow-1'
+          style={{ backgroundColor: 'rgba(255, 183, 0, 0.75)' }}>
+          <Entry name={prover} time={secondsPerMBTime} medal='goldRibbon' />
+        </li>
+      ))}
+      {entries.slice(1, 2).map(({ id, prover, secondsPerMBTime, params }) => (
+        <li
+          key={id}
+          className='tl f4 mt4 mh3 pa3 b--silver b--solid bw1 br3 shadow-1'
+          style={{ backgroundColor: 'rgba(153, 153, 153, 0.75)' }}>
+          <Entry name={prover} time={secondsPerMBTime} medal='silverRibbon' />
+        </li>
+      ))}
+      {entries.slice(2, 3).map(({ id, prover, secondsPerMBTime, params }) => (
+        <li
+          key={id}
+          className='tl f4 mt4 mh3 mb4 pa3 b--solid bw1 br3 shadow-1'
+          style={{ borderColor: '#cd7f32', backgroundColor: 'rgba(205, 127, 50, 0.75)' }}>
+          <Entry name={prover} time={secondsPerMBTime} medal='bronzeRibbon' />
+        </li>
+      ))}
+      {entries.slice(3).map(({ id, prover, secondsPerMBTime, params }) => (
+        <li key={id} className='tl mt3 mh3 ph3 f4'>
+          <Entry name={prover} time={secondsPerMBTime} />
+        </li>
+      ))}
+    </ol>
+    <div className='tc mb3'>
+      <button type='button' className='montserrat f6 fw2 gray bw0 br3 ph3 pv2 bg-black-10 hover-bg-black-30 pointer' onClick={onCollapse}>Minimise</button>
+    </div>
+  </div>
+)
+
+const Leaderboard = ({ entries }) => {
+  const [ expanded, setExpanded ] = useState(false)
+  return expanded
+    ? <ExpandedLeaderboard entries={entries} onCollapse={() => setExpanded(false)} />
+    : <CollapsedLeaderboard entries={entries} onExpand={() => setExpanded(true)} />
+}
+
+export default Leaderboard
